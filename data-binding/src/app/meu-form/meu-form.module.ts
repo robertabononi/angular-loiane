4062669cb97306d6d6f59825184fc52ca8f0adc1
robertabononi <roberta.bononi16@gmail.com>
2021-06-02 00:44:07 -0300

    chore: create structure for another module/component

---
+++ data-binding/src/app/meu-form/meu-form.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MeuFormComponent } from './meu-form.component';
+
+
+
+@NgModule({
+  declarations: [
+    MeuFormComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MeuFormModule { }
