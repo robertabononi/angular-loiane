a8eeaaf17a57b093ee4adb1e2b80060be372a251
robertabononi <roberta.bononi16@gmail.com>
2021-06-02 15:20:09 -0300

    chore: change the value of the view using it as reference

--- data-binding/src/app/output-property/output-property.component.ts
+++ data-binding/src/app/output-property/output-property.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'contador',
@@ -11,16 +11,15 @@ export class OutputPropertyComponent implements OnInit {
 
   @Output() mudouValor = new EventEmitter()
 
-  @ViewChild('campoInput', {static: false}) campoValorInput!: HTMLElement
+  @ViewChild('campoInput', {static: false}) campoValorInput!: ElementRef
 
   decrementa(){
-    console.log(this.campoValorInput)
-    this.valor--
+    this.campoValorInput.nativeElement.value--
     this.mudouValor.emit({novoValor: this.valor})
   }
 
   incrementa(){
-    this.valor++
+    this.campoValorInput.nativeElement.value++
     this.mudouValor.emit({novoValor: this.valor})
   }
 
