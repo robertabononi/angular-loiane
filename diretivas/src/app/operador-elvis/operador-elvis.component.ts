956c6bb5a65a80c1b65f308dc8e31630125de789
robertabononi <roberta.bononi16@gmail.com>
2021-06-09 21:54:02 -0300

    chore: use logic to write something when the object is null

--- diretivas/src/app/operador-elvis/operador-elvis.component.ts
+++ diretivas/src/app/operador-elvis/operador-elvis.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OperadorElvisComponent implements OnInit {
 
+  tarefa: any = {
+    descricao: 'Descrição da tarefa',
+    responsavel: null
+  }
+
   constructor() { }
 
   ngOnInit(): void {
