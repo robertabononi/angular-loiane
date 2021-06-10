6220ea2d7aa1b94295eb8aaa3bcaff172cecd5d4
robertabononi <roberta.bononi16@gmail.com>
2021-06-10 03:03:46 -0300

    chore: define pattern colors

--- diretivas/src/app/shared/highlight.directive.ts
+++ diretivas/src/app/shared/highlight.directive.ts
@@ -1,9 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
+
+  @HostListener('mouseenter') onMouseOver() {
+    this.backgroundColor = this.highlightColor
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.backgroundColor = this.defaultColor
+  }
+
+  @HostBinding('style.backgroundColor') backgroundColor?: string;
+
+  defaultColor: string = "white"
+  highlightColor: string = "yellow"
 
   constructor() { }
 
