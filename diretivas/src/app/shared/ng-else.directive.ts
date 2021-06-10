4d20ee398eb4e2072692a8386682239cc3060ae9
robertabononi <roberta.bononi16@gmail.com>
2021-06-10 03:51:31 -0300

    feat: show content and use the directive ngElse

--- diretivas/src/app/shared/ng-else.directive.ts
+++ diretivas/src/app/shared/ng-else.directive.ts
@@ -1,10 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
-  selector: '[appNgElse]'
+  selector: '[ngElse]'
 })
 export class NgElseDirective {
 
-  constructor() { }
+  @Input() set ngElse(condition: boolean){
+    if (!condition){
+      this._viewContainerRef.createEmbeddedView(
+        this._templateRef
+      );
+    } else {
+      this._viewContainerRef.clear();
+    }
+  }
+
+  constructor(
+    private _templateRef: TemplateRef<any>,
+    private _viewContainerRef: ViewContainerRef
+  ) { }
 
 }
