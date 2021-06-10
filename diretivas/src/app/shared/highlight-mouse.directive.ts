1970f53e5a0c2f3aae1c420b18b44e2b06839e4e
robertabononi <roberta.bononi16@gmail.com>
2021-06-10 02:08:56 -0300

    feat: apply and remove background color of HTML element when pass the mouse on it

--- diretivas/src/app/shared/highlight-mouse.directive.ts
+++ diretivas/src/app/shared/highlight-mouse.directive.ts
@@ -1,10 +1,29 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightMouse]'
 })
 export class HighlightMouseDirective {
 
-  constructor() { }
+  @HostListener('mouseenter') onMouseOver() {
+    this._renderer.setStyle(
+      this._elementRef.nativeElement,
+      'background-color',
+      'yellow'
+    )
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this._renderer.setStyle(
+      this._elementRef.nativeElement,
+      'background-color',
+      'white'
+    )
+  }
+
+  constructor(
+    private _elementRef: ElementRef,
+    private _renderer: Renderer2
+  ) { }
 
 }
