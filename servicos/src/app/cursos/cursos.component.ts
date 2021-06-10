994f2c00c35b8c570e0bd00e22dd7edf6820bfdb
robertabononi <roberta.bononi16@gmail.com>
2021-06-10 10:51:14 -0300

    feat: show static data in the view

--- servicos/src/app/cursos/cursos.component.ts
+++ servicos/src/app/cursos/cursos.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
+  cursos: string[] = ['Angular 2', 'Java', 'Phonegap'];
+
   constructor() { }
 
   ngOnInit(): void {
