78d119e3fdd3a945d9f0b1b23f72b112b01062ad
robertabononi <roberta.bononi16@gmail.com>
2021-06-10 18:46:01 -0300

    chore: create different instances for each component

--- servicos/src/app/criar-curso/criar-curso.component.ts
+++ servicos/src/app/criar-curso/criar-curso.component.ts
@@ -4,7 +4,8 @@ import { CursosService } from '../cursos/cursos.service';
 @Component({
   selector: 'app-criar-curso',
   templateUrl: './criar-curso.component.html',
-  styleUrls: ['./criar-curso.component.css']
+  styleUrls: ['./criar-curso.component.css'],
+  providers: [CursosService]
 })
 export class CriarCursoComponent implements OnInit {
 
