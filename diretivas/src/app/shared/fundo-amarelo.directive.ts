abc9e34a68e9b94a2858d494642bb2d8e211585b
robertabononi <roberta.bononi16@gmail.com>
2021-06-10 01:37:20 -0300

    refactor: use Renderer2 to set value to the property because it's safer

--- diretivas/src/app/shared/fundo-amarelo.directive.ts
+++ diretivas/src/app/shared/fundo-amarelo.directive.ts
@@ -5,9 +5,17 @@ import { Directive, ElementRef, Renderer2 } from '@angular/core';
 })
 export class FundoAmareloDirective {
 
-  constructor(private _elementRef: ElementRef) {
+  constructor(
+    private _elementRef: ElementRef,
+    private _renderer: Renderer2
+  ) {
     //console.log(this._elementRef)
-    this._elementRef.nativeElement.style.backgroundColor = "yellow";
+    //this._elementRef.nativeElement.style.backgroundColor = "yellow";
+    this._renderer.setStyle(
+      this._elementRef.nativeElement,
+      'background-color',
+      'yellow'
+    )
   }
 
 }
