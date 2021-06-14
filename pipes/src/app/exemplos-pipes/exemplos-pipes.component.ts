22e522aad3fc9a024810e0ebaf126fd42e764421
robertabononi <roberta.bononi16@gmail.com>
2021-06-14 13:34:14 -0300

    feat: create new object 'livro' and show its infos in the view

--- pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
+++ pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExemplosPipesComponent implements OnInit {
 
+  livro: any = {
+    titulo: 'Assassinato no expresso do oriente',
+    rating: 4.76543,
+    numeroPaginas: 200,
+    preco: 34.66,
+    dataLancamento: new Date(2017, 6, 3),
+    url: 'https://www.amazon.com.br/Assassinato-no-Expresso-do-Oriente/dp/8595080631'
+  };
+
   constructor() { }
 
   ngOnInit(): void {
