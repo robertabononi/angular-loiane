a1efae120e6141e61e01c4005e3127ae11323f8d
robertabononi <roberta.bononi16@gmail.com>
2021-06-15 20:43:42 -0300

    feat: create list of 'cursos' and show their details based on id

--- rotas/src/app/curso-detalhe/curso-detalhe.component.ts
+++ rotas/src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { CursosService } from './../cursos/cursos.service';
 
 @Component({
   selector: 'app-curso-detalhe',
@@ -8,14 +10,20 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CursoDetalheComponent implements OnInit {
 
-  cursoId!: string;
+  cursoId!: number;
+  inscricao!: Subscription;
+  curso: any;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(
+    private route: ActivatedRoute,
+    private cursosService: CursosService
+    ) {
     this.cursoId = this.route.snapshot.params['id'];
     //console.log(this.route);
    }
 
   ngOnInit(): void {
+    this.curso = this.cursosService.getCurso(this.cursoId)
   }
 
 }
--- rotas/src/app/cursos/cursos.service.ts
+++ rotas/src/app/cursos/cursos.service.ts
@@ -5,5 +5,22 @@ import { Injectable } from '@angular/core';
 })
 export class CursosService {
 
+  getCursos(){
+    return[
+      {idCurso: 1, nome: 'Angular'},
+      {idCurso: 2, nome: 'Java'}
+    ]
+  }
+
+  getCurso(id: number){
+    let cursos = this.getCursos();
+    for (let curso of cursos){
+      if (curso.idCurso == id){
+        return curso;
+      }
+    }
+    return null;
+  }
+
   constructor() { }
 }
