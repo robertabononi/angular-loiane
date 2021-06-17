293eb6c365baf3370c29954ae6a7fa7836bf67f6
robertabononi <roberta.bononi16@gmail.com>
2021-06-17 13:01:40 -0300

    refactor: create new routing module for 'cursos' to let the app more organized

--- rotas/src/app/app-routing.module.ts
+++ rotas/src/app/app-routing.module.ts
@@ -3,16 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from "./login/login.component";
-import { CursosComponent } from './cursos/cursos.component';
-import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
-import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'cursos', component: CursosComponent },
-  { path: 'curso/:id', component: CursoDetalheComponent},
-  { path: 'naoEncontrado', component: CursoNaoEncontradoComponent}
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
--- rotas/src/app/cursos/cursos.module.ts
+++ rotas/src/app/cursos/cursos.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 
 import { CursosComponent } from './cursos.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 import { CursosService } from './cursos.service';
+import { CursosRoutingModule } from './cursos-routing.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +15,7 @@ import { CursosService } from './cursos.service';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    CursosRoutingModule
   ],
   providers: [
     CursosService
