e02cc169a12fe09b773316c337ce665a891246b1
robertabononi <roberta.bononi16@gmail.com>
2021-06-17 16:42:28 -0300

    feat: create children components and create routes for them

--- rotas/src/app/alunos/alunos-routing.module.ts
+++ rotas/src/app/alunos/alunos-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AlunosComponent } from './alunos.component';
+import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
+import { AlunoFormComponent } from './aluno-form/aluno-form.component';
+
+
+const routes: Routes = [
+  {path: 'alunos', component: AlunosComponent},
+  {path: 'alunos/novo', component: AlunoFormComponent},
+  {path: 'alunos/:id', component: AlunoDetalheComponent},
+  {path: 'alunos/:id/editar', component: AlunoFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- rotas/src/app/alunos/alunos.module.ts
+++ rotas/src/app/alunos/alunos.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { AlunosRoutingModule } from './alunos-routing.module';
 import { AlunosComponent } from './alunos.component';
+import { AlunoFormComponent } from './aluno-form/aluno-form.component';
+import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
 
 
 @NgModule({
   declarations: [
-    AlunosComponent
+    AlunosComponent,
+    AlunoFormComponent,
+    AlunoDetalheComponent
   ],
   imports: [
     CommonModule,
