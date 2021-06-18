8babca331675a86270638f18d1bb636a6b1d27bb
robertabononi <roberta.bononi16@gmail.com>
2021-06-18 16:37:39 -0300

    feat: create and authenticate login

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-
+import { AuthService } from './login/auth.service';
 
 @NgModule({
   declarations: [
@@ -16,10 +17,11 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     TooltipModule.forRoot(),
     AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
