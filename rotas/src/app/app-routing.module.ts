2a402c5a05463e355333aafa611476e2538e0474
robertabononi <roberta.bononi16@gmail.com>
2021-06-18 20:47:00 -0300

    chore: use canActivate in guard to allow access only if login is done

--- rotas/src/app/app-routing.module.ts
+++ rotas/src/app/app-routing.module.ts
@@ -1,20 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthGuard } from './guard/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from "./login/login.component";
 
 const routes: Routes = [
   {
     path:'cursos',
-    loadChildren: () => import('./cursos/cursos.module').then(m => m.CursosModule)
+      loadChildren: () => import('./cursos/cursos.module').then(m => m.CursosModule),
+      canActivate: [AuthGuard]
   },
   {
     path:'alunos',
-    loadChildren: () => import('./alunos/alunos.module').then(m => m.AlunosModule)
+    loadChildren: () => import('./alunos/alunos.module').then(m => m.AlunosModule),
+    canActivate: [AuthGuard]
   },
-  { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent }
+  {
+    path: '',
+    component: HomeComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
 ];
 
 @NgModule({
--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AuthService } from './login/auth.service';
+import { AuthGuard } from './guard/auth.guard';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,7 @@ import { AuthService } from './login/auth.service';
     TooltipModule.forRoot(),
     AppRoutingModule
   ],
-  providers: [AuthService],
+  providers: [AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
