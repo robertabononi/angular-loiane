9b2158ae8987a1024f07c2551f15e2d66d760554
robertabononi <roberta.bononi16@gmail.com>
2021-06-24 16:59:45 -0300

    style: use css classes applied in controllers to style fields in case the are invalid

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class TemplateFormComponent implements OnInit {
 
   usuario:any = {
-    nome: 'Roberta',
-    email: 'roberta@email.com'
+    nome: '',
+    email: ''
   }
 
   onSubmit(form: any){
