9a4a29496bb7b941999da771eb685e23e71e81f4
robertabononi <roberta.bononi16@gmail.com>
2021-06-29 10:12:34 -0300

    chore: fill address fields automatically and reset it in case already has something written

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -30,7 +30,7 @@ export class TemplateFormComponent implements OnInit {
     return !campo.valid && campo.touched
   }
 
-  consultaCEP(cep: any) {
+  consultaCEP(cep: any, form: any) {
 
     cep = cep.replace(/\D/g, '');
 
@@ -40,10 +40,54 @@ export class TemplateFormComponent implements OnInit {
 
       if(validaCep.test(cep)) {
 
+        this.resetaDadosForm(form)
+
         this.http.get(`https://viacep.com.br/ws/${cep}/json`)
-        .subscribe(dados => console.log(dados));
+        .subscribe(dados => this.populaDadosForm(dados, form));
 
       }
     }
+  }
+
+  populaDadosForm(dados: any, formulario: any){
+    /* formulario.setValue({
+      nome: formulario.value.nome,
+      email: formulario.value.email,
+      endereco: {
+        cep:dados.cep,
+        rua: dados.logradouro,
+        numero: '',
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    }) */
+
+    formulario.form.patchValue({
+      endereco: {
+        cep:dados.cep,
+        rua: dados.logradouro,
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    })
+
+    //console.log(formulario)
+
+  }
+
+  resetaDadosForm(formulario: any){
+    formulario.form.patchValue({
+      endereco: {
+        rua: null,
+        complemento: null,
+        bairro:null,
+        cidade: null,
+        estado: null
+      }
+    });
   }
 }
