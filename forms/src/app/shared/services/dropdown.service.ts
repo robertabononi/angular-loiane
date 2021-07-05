3ad906a7a9f5586de351a53e6454481ae8df1ffc
robertabononi <roberta.bononi16@gmail.com>
2021-07-05 15:21:44 -0300

    chore: create new json's file with brazilian states and made http DI on DropDownService

--- forms/src/app/shared/services/dropdown.service.ts
+++ forms/src/app/shared/services/dropdown.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DropdownService {
 
-constructor() { }
+  constructor(http: HttpClient) { }
 
+  getEstadosBr() {
+
+  }
 }
--- forms/src/app/shared/shared.module.ts
+++ forms/src/app/shared/shared.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { FormDebugComponent } from './form-debug/form-debug.component';
 import { DropdownService } from './services/dropdown.service';
 
 @NgModule({
   declarations: [FormDebugComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    HttpClientModule
+  ],
   exports: [FormDebugComponent],
   providers: [DropdownService]
 })
