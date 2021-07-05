a5217e7d90c205195425135ec3575339a6762dce
robertabononi <roberta.bononi16@gmail.com>
2021-07-05 15:01:57 -0300

    chore: create new shared service for dropdown

--- forms/src/app/shared/shared.module.ts
+++ forms/src/app/shared/shared.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FormDebugComponent } from './form-debug/form-debug.component';
+import { DropdownService } from './services/dropdown.service';
 
 @NgModule({
   declarations: [FormDebugComponent],
-  imports: [
-    CommonModule
-  ],
-  exports: [FormDebugComponent]
+  imports: [CommonModule],
+  exports: [FormDebugComponent],
+  providers: [DropdownService]
 })
 export class SharedModule { }
