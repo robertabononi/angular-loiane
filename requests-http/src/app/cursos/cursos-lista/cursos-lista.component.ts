578c1fd5becfce65f6b4834d3459b08d3ec8274b
robertabononi <roberta.bononi16@gmail.com>
2021-07-19 15:28:50 -0300

    feat: create method 'list()' using get from API Rest and applied it in a table on template

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CursosService } from '../cursos.service';
+import { Curso } from './curso';
+
 @Component({
   selector: 'app-cursos-lista',
   templateUrl: './cursos-lista.component.html',
@@ -7,9 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosListaComponent implements OnInit {
 
-  constructor() { }
+  cursos!: Curso[];
+
+  constructor(private service: CursosService) { }
 
   ngOnInit(): void {
+    this.service.list().subscribe(dados => this.cursos = dados);
   }
 
 }
---
+++ requests-http/src/app/cursos/cursos.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Curso } from './cursos-lista/curso';
+import { tap } from 'rxjs/operators'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CursosService {
+
+  private readonly API = "http://localhost:3000/cursos";
+
+  constructor(private http: HttpClient) { }
+
+  list() {
+    return this.http.get<Curso[]>(this.API)
+      .pipe(
+        tap(console.log)
+      )
+    ;
+  }
+}
