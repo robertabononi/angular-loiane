f10e4bed64168423272d761f7f7d0cca5ab3e26d
robertabononi <roberta.bononi16@gmail.com>
2021-07-20 01:19:15 -0300

    feat: show error message in case of the http doesn't work

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Observable } from 'rxjs';
+import { EMPTY, Observable, Subject } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { CursosService } from '../cursos.service';
 import { Curso } from './curso';
 
@@ -13,13 +14,25 @@ export class CursosListaComponent implements OnInit {
 
   //cursos!: Curso[];
   cursos$!: Observable<Curso[]>;
+  error$ = new Subject<boolean>();
 
   constructor(private service: CursosService) { }
 
   ngOnInit(): void {
     //this.service.list().subscribe(dados => this.cursos = dados);
+    this.onRefresh();
+  }
 
-    this.cursos$ = this.service.list();
+  onRefresh() {
+    this.cursos$ = this.service.list()
+      .pipe(
+        catchError(error => {
+          console.error(error);
+          this.error$.next(true)
+          return EMPTY;
+        })
+      )
+    ;
   }
 
 }
