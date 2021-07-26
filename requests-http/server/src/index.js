06f38308197452fdee251e66c934912c0110bceb
robertabononi <roberta.bononi16@gmail.com>
2021-07-26 20:47:39 -0300

    fix: correct typo

--- requests-http/server/src/index.js
+++ requests-http/server/src/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+//import cors from 'cors';
+//import bodyParser from 'body-parser';
 const multipart = require('connect-multiparty');
 
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 /* const corsOptions = {
   origin: '*',
@@ -15,7 +15,7 @@ app.use(cors(corsOptions)); */
 
 const multipartMiddleware = multipart({ uploadDir: './uploads' });
 app.post('/upload', multipartMiddleware, (req, res) => {
-  const files = req.fies;
+  const files = req.files;
   console.log(files);
   res.json({ message: files });
 })
