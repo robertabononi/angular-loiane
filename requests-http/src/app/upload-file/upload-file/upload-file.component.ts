1eea36e55694c88c6cd0eed0aaac09240c4d1478
robertabononi <roberta.bononi16@gmail.com>
2021-07-26 12:54:04 -0300

    chore: upload and save files from the input

--- requests-http/src/app/upload-file/upload-file/upload-file.component.ts
+++ requests-http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UploadFileService } from '../upload-file.service';
 
 @Component({
   selector: 'app-upload-file',
   templateUrl: './upload-file.component.html',
   styleUrls: ['./upload-file.component.css']
 })
-export class UploadFileComponent implements OnInit {
+export class UploadFileComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  files!: Set<File>;
+  sub!: Subscription;
+
+  constructor(private service: UploadFileService) { }
 
   ngOnInit() {
+  }
+
+  onChange(event: any) {
+    console.log(event)
+
+    const selectedFiles = <FileList>event.srcElement.files;
+    const output = document.getElementById('formFileMultiple');
+    /* if (output) {
+      output.innerHTML = selectedFiles[0].name;
+    } */
+
+    const fileNames = [];
+    this.files = new Set();
+    for (let i = 0; i < selectedFiles.length; i++) {
+      fileNames.push(selectedFiles[i].name);
+      this.files.add(selectedFiles[i]);
+    }
+    if (output) {
+      output.innerHTML = fileNames.join(', ');
+    }
+  }
+
+  onUpload() {
+    if(this.files && this.files.size > 0) {
+      this.sub = this.service.upload(this.files, 'http://localhost:8000/upload')
+        .subscribe(response => console.log('Upload concluído'));
+    }
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
 }
