9ee8702ae18822f5c59cf9d65a137423196c4cfd
robertabononi <roberta.bononi16@gmail.com>
2021-07-27 12:19:29 -0300

    refactor: create customized rxjs operator to simplify code

--- requests-http/src/app/upload-file/upload-file/upload-file.component.ts
+++ requests-http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,6 +1,7 @@
 import { HttpEvent, HttpEventType } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { filterResponse, uploadProgress } from 'src/app/shared/rxjs-operators';
 import { environment } from 'src/environments/environment';
 import { UploadFileService } from '../upload-file.service';
 
@@ -45,19 +46,27 @@ export class UploadFileComponent implements OnInit, OnDestroy {
   onUpload() {
     if(this.files && this.files.size > 0) {
       this.sub = this.service.upload(this.files, `${environment.BASE_URL}/upload`)
-        .subscribe((event: HttpEvent<Object>) => {
-          //HttpEventType
-          console.log(event);
+        .pipe(
+          uploadProgress(progress => {
+            console.log(progress);
+            this.progress = progress;
+          }),
+          filterResponse()
+        )
+        .subscribe(response => console.log('Upload concluído'))
+      ;
+      /* .subscribe((event: HttpEvent<Object>) => {
+          //console.log(event);
 
           if (event.type == HttpEventType.Response) {
             console.log('Upload concluído');
           } else if (event.type == HttpEventType.UploadProgress) {
             const percentDone = Math.round((event.loaded * 100) / event.total!);
-            console.log('Progresso: ', percentDone);
+            //console.log('Progresso: ', percentDone);
             this.progress = percentDone;
           }
         })
-      ;
+      ; */
     }
   }
 
