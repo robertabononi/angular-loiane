040bbfb94b8575494ea45004e57df15db33ef2d9
robertabononi <roberta.bononi16@gmail.com>
2021-07-28 01:16:01 -0300

    feat: use cdnjs' api to search for results by its encode

--- requests-http/src/app/reactive-search/reactive-search/reactive-search.component.ts
+++ requests-http/src/app/reactive-search/reactive-search/reactive-search.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-reactive-search',
@@ -9,14 +12,23 @@ import { FormControl } from '@angular/forms';
 export class ReactiveSearchComponent implements OnInit {
 
   queryField = new FormControl();
+  readonly SEARCH_URL = 'https://api.cdnjs.com/libraries';
+  results$!: Observable<any>;
+  total = 0;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
   onSearch() {
     console.log(this.queryField.value)
+
+    this.results$ = this.http.get(`${this.SEARCH_URL}?fields=name,description,version,homepage&search=angular`)
+      .pipe(
+        tap((res: any) => this.total = res.total),
+        map((res: any) => res.results)
+      )
   }
 
 }
