2a7c5b418ab7d0118429a64e9616242a1d649139
robertabononi <roberta.bononi16@gmail.com>
2021-07-28 02:25:42 -0300

    chore: use reactive and functional coding to search in a reactive way

--- requests-http/src/app/reactive-search/reactive-search/reactive-search.component.ts
+++ requests-http/src/app/reactive-search/reactive-search/reactive-search.component.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { map, tap, filter, distinctUntilChanged, debounceTime, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-reactive-search',
@@ -15,25 +15,41 @@ export class ReactiveSearchComponent implements OnInit {
   readonly SEARCH_URL = 'https://api.cdnjs.com/libraries';
   results$!: Observable<any>;
   total = 0;
+  readonly FIELDS = 'name,description,version,homepage';
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.results$ = this.queryField.valueChanges
+    .pipe(
+      map(value => value.trim()),
+      filter(value => value.length > 1),
+      debounceTime(200),
+      distinctUntilChanged(),
+      //tap(value => console.log(value)),
+      switchMap(value => this.http.get(this.SEARCH_URL, {
+        params: {
+          search: value,
+          fields: this.FIELDS
+        }
+      })),
+      tap((res: any) => this.total = res.total),
+      map((res: any) => res.results)
+    )
   }
 
   onSearch() {
-    const fields = 'name,description,version,homepage';
     let value = this.queryField.value;
     if (value && (value = value.trim()) !== '') {
 
       const params_ = {
         search: value,
-        fields: fields
+        fields: this.FIELDS
       }
 
       let params = new HttpParams();
       params = params.set('search', value);
-      params = params.set('fields', fields)
+      params = params.set('fields', this.FIELDS)
 
       this.results$ = this.http
         .get(this.SEARCH_URL, { params })
