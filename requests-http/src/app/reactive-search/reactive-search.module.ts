21eeaac250eab8496e3e0c67de3fad00c5e97e8d
robertabononi <roberta.bononi16@gmail.com>
2021-07-28 00:46:02 -0300

    feat: create card structure to search for something

--- requests-http/src/app/reactive-search/reactive-search.module.ts
+++ requests-http/src/app/reactive-search/reactive-search.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ReactiveSearchRoutingModule } from './reactive-search-routing.module';
 import { ReactiveSearchComponent } from './reactive-search/reactive-search.component';
@@ -11,6 +12,7 @@ import { ReactiveSearchComponent } from './reactive-search/reactive-search.compo
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     ReactiveSearchRoutingModule
   ]
 })
--- requests-http/src/app/reactive-search/reactive-search/reactive-search.component.ts
+++ requests-http/src/app/reactive-search/reactive-search/reactive-search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-search',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveSearchComponent implements OnInit {
 
+  queryField = new FormControl();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSearch() {
+    console.log(this.queryField.value)
   }
 
 }
